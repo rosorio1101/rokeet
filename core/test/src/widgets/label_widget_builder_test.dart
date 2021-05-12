c6cf582d30fc69f4f4cdc8c1443200cb973ab4dd
roberto osorio <robertoosorio.s@gmail.com>
2021-05-12 19:41:06 -0400

    add tests

--- core/test/src/widgets/label_widget_builder_test.dart
+++ core/test/src/widgets/label_widget_builder_test.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:rokeetui_core/core.dart';
 import 'package:rokeetui_core/src/widgets/widget.label.dart';
 
 import '../utils.dart';
 
-@GenerateMocks([RState, BuildContext])
 void main() {
   RLabelWidgetBuilder? builder;
   Rokeet? rokeet;
-  group('Widget Label Test', () {
+  group('Label Widget Builder Test', () {
     setUp(() {
       builder = RLabelWidgetBuilder();
       rokeet = Rokeet();
     });
 
     testWidgets('Builder should build a Label with Data', (tester) async {
-      var label = RLabelWidget.fromJson(loadJson('widgets/label_builder/label'));
+      var label = RLabelWidget.fromJson(loadJson('widgets/label'));
       var labelWidget =  builder!.build(rokeet!, label) as Text;
       expect(label.data!.text, labelWidget.data!);
     });
