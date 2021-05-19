21d093cf20d42f673f80265e0ccb901548feef12
osodroid <robertoosorio.s@gmail.com>
2021-05-18 20:13:24 -0400

    add stack to context and state for correct navigation and action performers (#17)

--- core/lib/src/constants.dart
+++ core/lib/src/constants.dart
@@ -1,16 +1,15 @@
-
 import 'dart:io';
 
 var baseUrl = _getBaseUrl();
 
 String _getBaseUrl() {
-  try{
-    if(Platform.isAndroid) {
+  try {
+    if (Platform.isAndroid) {
       return 'https://10.0.2.2:3000';
     } else {
       return 'https://dev.rokeet.io:3000';
     }
-  } catch(e){
+  } catch (e) {
     return 'https://dev.rokeet.io:3000';
   }
 }
--- core/lib/src/pages/page_step.dart
+++ core/lib/src/pages/page_step.dart
@@ -14,28 +14,21 @@ class RokeetStepPage extends AbstractRokeetPage {
 
 class _RokeetStepPageState extends RState<RokeetStepPage, RStep> {
   @override
-  void initState() {
-    super.initState();
-    rokeet.currentState = this;
-    rokeet.currentContext = context;
-  }
-
-  Widget _getPage() => FutureBuilder<RStep?>(
+  Widget buildPage(BuildContext context) => FutureBuilder<RStep?>(
       future: rokeet.getStep(widget.stepId),
       builder: (context, snapshot) {
-        if(snapshot.connectionState != ConnectionState.done) {
+        if (snapshot.connectionState != ConnectionState.done) {
           return getLoadingWidget();
         }
 
         var data = snapshot.data;
         var body = rokeet.buildWidget(data!.body!)!;
-        return Scaffold(body: SafeArea(
-          child: body,
-        ));
-      }
-  );
-
-  @override
-  Widget build(BuildContext context) => _getPage();
-
+        Key key = Key(data.id!);
+        return Scaffold(
+          body: SafeArea(
+            child: body,
+          ),
+          key: key,
+        );
+      });
 }
