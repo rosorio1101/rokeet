4abbc6a7d4f01fc94e1976fc52fbcca60b796b52
osodroid <robertoosorio.s@gmail.com>
2022-03-14 20:41:52 -0300

    update constants to support ios (#21)
    * update constants to support ios

--- core/lib/src/constants.dart
+++ core/lib/src/constants.dart
@@ -2,10 +2,39 @@ import 'dart:io';
 
 var baseUrl = _getBaseUrl();
 
-String _getBaseUrl() {
+enum Environment { PRODUCTION, DEBUG }
+
+String _getBaseUrl({Environment env = Environment.PRODUCTION}) {
+  switch (env) {
+    case Environment.PRODUCTION:
+      return _getProductionUrl();
+    default:
+      return _getDebugUrl();
+  }
+}
+
+String _getProductionUrl() {
   try {
     if (Platform.isAndroid) {
       return 'https://10.0.2.2:3000';
+    }
+    if (Platform.isIOS) {
+      return 'http://localhost:3000';
+    } else {
+      return 'https://dev.rokeet.io:3000';
+    }
+  } catch (e) {
+    return 'https://dev.rokeet.io:3000';
+  }
+}
+
+String _getDebugUrl() {
+  try {
+    if (Platform.isAndroid) {
+      return 'https://10.0.2.2:3000';
+    }
+    if (Platform.isIOS) {
+      return 'http://localhost:3000';
     } else {
       return 'https://dev.rokeet.io:3000';
     }
