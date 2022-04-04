1148c2a1d09774a5a02a5bc7b4caa7d4dfcd0c22
osodroid <robertoosorio.s@gmail.com>
2022-04-04 19:02:55 -0400

    fix android (#29)

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var rokeet = RokeetBuilder()
-        .withBaseUrl('http://localhost:3000')
+        .withBaseUrl('http://10.0.2.2:3000')
         .withConfig(config)
         .build();
     return RokeetApp(
